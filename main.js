9ad263ed1d0ab196c69e11a245d62d1b8febe6f1
pinkcustard2 <35869034+pinkcustard2@users.noreply.github.com>
2018-02-24 18:00:28 +0000

    Added progress saving

--- main.js
+++ main.js
@@ -1,20 +1,34 @@
 var orangeTotalEl = document.getElementById("Oranges");
+//localStorage.removeItem("oranges");
 
 var oranges = 0; //The number of oranges the player has
+if (localStorage.getItem("oranges") === null) {
+	var oranges = 0;
+}
+else{
+	oranges = parseInt(localStorage.getItem("oranges"));
+}
+
 
 var clickPower = 1
 
-orangeTotalEl.innerHTML = "Oranges: 0";
+orangeTotalEl.innerHTML = "Oranges: " + oranges;
 
 //orangeClicked();
 
 function orangeClicked() {
 	oranges+=clickPower;
 	orangeTotalEl.innerHTML = "Oranges: " + oranges;
+	localStorage.setItem("oranges", oranges);
 }
 
 
 
 function upgradeClickPower(upgradeAmount) {
 	clickPower+=upgradeAmount;
+}
+
+function dismissText() {
+	var storeEl = document.getElementById("storageNote");
+	storeEl.style.visibility = "hidden";
 }
