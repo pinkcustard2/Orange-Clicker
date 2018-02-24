cc3f60b4615777ee2721f794b787ab54d2e2f994
pinkcustard2 <35869034+pinkcustard2@users.noreply.github.com>
2018-02-24 18:49:04 +0000

    Added upgrades
    More updates soon!
    
    Co-Authored-By: Jake Thakur <jakethakur22@users.noreply.github.com>

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 var orangeTotalEl = document.getElementById("Oranges");
 //localStorage.removeItem("oranges");
 
-var oranges = 0; //The number of oranges the player has
 if (localStorage.getItem("oranges") === null) {
-	var oranges = 0;
+	var oranges = 0; //The number of oranges the player has
 }
 else{
-	oranges = parseInt(localStorage.getItem("oranges"));
+	var oranges = parseInt(localStorage.getItem("oranges"));
 }
 
 if (localStorage.getItem("dismissed") === null) {
@@ -16,7 +15,23 @@ else{
 	dismissText();
 }
 
-var clickPower = 1
+if (localStorage.getItem("clickPower") === null) {
+	var clickPower = 1;
+}
+else{
+	var clickPower = parseInt(localStorage.getItem("clickPower"));
+}
+
+if (localStorage.getItem("upgrades") === null) {
+	var upgrades = 0;
+}
+else{
+	var upgrades = parseInt(localStorage.getItem("upgrades"));
+}
+
+if(upgrades >= 1) {
+	hide("upgrade1");
+}
 
 orangeTotalEl.innerHTML = "Oranges: " + oranges;
 
@@ -28,14 +43,31 @@ function orangeClicked() {
 	localStorage.setItem("oranges", oranges);
 }
 
-
-
-function upgradeClickPower(upgradeAmount) {
-	clickPower+=upgradeAmount;
+function upgradeClickPower(upgradeAmount, price, elementId) {
+	if (oranges >= price){
+		clickPower+=upgradeAmount;
+		oranges-= price;
+		orangeTotalEl.innerHTML = "Oranges: " + oranges;
+		localStorage.setItem("oranges", oranges);
+		localStorage.setItem("clickPower", clickPower);
+		hide(elementId);
+		upgrades++;
+		localStorage.setItem("upgrades", upgrades);
+	}
+	else {
+		alert("You do not have enough oranges. You need " + (price - oranges) + " more oranges to buy this.");
+	}
+	
 }
 
 function dismissText() {
-	var storeEl = document.getElementById("storageNote");
-	storeEl.style.visibility = "hidden";
+	hide("storageNote");
 	localStorage.setItem("dismissed", true);
 }
+
+function hide(elementId){
+	var El = document.getElementById(elementId);
+	El.style.visibility = "hidden";
+}
+
+
