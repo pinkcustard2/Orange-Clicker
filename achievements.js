290d71e654c1ad747174402ee56d292dee3b167a
pinkcustard2 <35869034+pinkcustard2@users.noreply.github.com>
2018-03-03 11:58:03 +0000

    Added achievements

--- achievements.js
+++ achievements.js
@@ -5,7 +5,7 @@ if (localStorage.getItem("achievementsEarned") === null) {
 	}
 }
 else{
-	var achievementsEarned = parseInt(localStorage.getItem("achievementsEarned"));
+	var achievementsEarned = JSON.parse(localStorage.getItem("achievementsEarned"));
 }
 
 achievementsEarned[0] = true;
@@ -14,5 +14,30 @@ achievementsUpdate("achievement0");
 function achievementsUpdate(elementId){
 	var El = document.getElementById(elementId);
 	El.innerHTML = "Earned"
-	localStorage.setItem("achievementsEarned", achievementsEarned);
+	localStorage.setItem("achievementsEarned", JSON.stringify(achievementsEarned));
+}
+
+if (achievementsEarned[1] == true){
+	achievementsUpdate("achievement1");
+}
+
+if (achievementsEarned[2] == true){
+	achievementsUpdate("achievement2");
+}
+
+if (achievementsEarned[3] == true){
+	achievementsUpdate("achievement3");
+}
+
+
+if (achievementsEarned[4] == true){
+	achievementsUpdate("achievement4");
+}
+
+if (achievementsEarned[5] == true){
+	achievementsUpdate("achievement5");
+}
+
+if (achievementsEarned[6] == true){
+	achievementsUpdate("achievement6");
 }
--- main.js
+++ main.js
@@ -1,9 +1,25 @@
 var orangeTotalEl = document.getElementById("Oranges");
 var clickPowerEl = document.getElementById("ClickPower");
 var OPSEl = document.getElementById("OPS");
+var news = [
+	"This update added news",
+	"The game is in version 0.4.3",
+	"You enjoy making oranges but nobody likes them",
+	"You have discovered a new way to make oranges quicker",
+	"Your family enjoys your oranges",
+	"Your road enjoys your oranges",
+	"Your town enjoys your oranges",
+	"Scientists wonder how you produce oranges so quickly",
+];
+
+var newsRequirements = [
+	
+]
+
 //localStorage.removeItem("oranges");
 //localStorage.removeItem("clickPower");
 //localStorage.removeItem("upgrades");
+//localStorage.removeItem("achievementsEarned");
 
 
 
@@ -34,6 +50,16 @@ if (localStorage.getItem("clickPower") === null) {
 }
 else{
 	var clickPower = parseInt(localStorage.getItem("clickPower"));
+}
+
+if (localStorage.getItem("achievementsEarned") === null) {
+	var achievementsEarned = [];
+	for(var i = 0; i < 499; i++){
+		achievementsEarned.push(false);
+	}
+}
+else{
+	var achievementsEarned = JSON.parse(localStorage.getItem("achievementsEarned"));
 }
 
 if(clickPower >= 2) {
@@ -99,7 +125,7 @@ function hide(elementId){
 }
 
 function harvestOranges(){
-	oranges+= OPS;
+	oranges+=OPS;
 	updateOranges();
 }
 
@@ -107,6 +133,36 @@ function updateOranges(){
 	orangeTotalEl.innerHTML = "Oranges: " + oranges;
 	localStorage.setItem("oranges", oranges);
 	document.title = "Oranges: " + oranges;
+	if (oranges >= 1 && achievementsEarned[1] == false){
+		achievementsEarned[1] = true;
+		localStorage.setItem("achievementsEarned", JSON.stringify(achievementsEarned));
+		alert("You got the achievement 'Getting Started'. View the achievements menu to see all the other achievements.");
+	}
+	if (clickPower >= 1 && achievementsEarned[2] == false){
+		achievementsEarned[2] = true;
+		localStorage.setItem("achievementsEarned", JSON.stringify(achievementsEarned));
+		alert("You got the achievement 'Getting An Upgrade'. Collect more oranges or buy more upgrades to receive more achievements.");
+	}
+	if (oranges >= 250 && achievementsEarned[3] == false){
+		achievementsEarned[3] = true;
+		localStorage.setItem("achievementsEarned", JSON.stringify(achievementsEarned));
+		alert("You got the achievement 'A Crate Full Of Oranges'. The next one will be a bit harder.");
+	}
+	if (oranges >= 2500 && achievementsEarned[4] == false){
+		achievementsEarned[4] = true;
+		localStorage.setItem("achievementsEarned", JSON.stringify(achievementsEarned));
+		alert("You got the achievement 'A Truck Full Of Oranges'.");
+	}
+	if (oranges >= 10000 && achievementsEarned[5] == false){
+		achievementsEarned[5] = true;
+		localStorage.setItem("achievementsEarned", JSON.stringify(achievementsEarned));
+		alert("You got the achievement 'A Ton Of Oranges'.");
+	}
+	if (clickPower >= 15 && achievementsEarned[6] == false){
+		achievementsEarned[6] = true;
+		localStorage.setItem("achievementsEarned", JSON.stringify(achievementsEarned));
+		alert("You got the achievement 'Scientists Are Interested'.");
+	}
 }
 
 function updateClickPower(){
